f54bb2104d0bddcd9ee363ef04e4df1235e47b2c
Pranav <pranavm1932@gmail.com>
2017-07-11 15:36:37 +0530

    Adding More comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## makeCacheMatrix creates a special matrix object with getters and setters for the 
+## matrix value and its inverse
+
+## cache solve takes the special matrix object as argument and returns the inverse
+## Computes the inverse the first time the first time the function is called and stores it
+## Returns the stored value as long as the matrix value doesn't change
+
 ## Write a short comment describing this function
 ## Creates the special matrix object with getters and setters for inverse and the matrix value 
 ## Takes and matrix as argument and returns the special object which is a list
@@ -21,6 +28,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 ## Takes the special matrix object as argument and returns the inverse of the matrix
+## Caches the computed result and returns the cached result on subsequent calls
+
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     i <- x$getInverse()
